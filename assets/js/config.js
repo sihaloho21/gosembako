d1498439639290b1f753bbd9250a03ccc1e490d5
sihaloho21 <ridohaloho.yt@gmail.com>
2026-01-21 11:06:51 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -7,8 +7,8 @@
 const CONFIG = {
     // Default API URLs
     DEFAULTS: {
-        MAIN_API: 'https://sheetdb.io/api/v1/f1ioa83a268s8',
-        ADMIN_API: 'https://sheetdb.io/api/v1/f1ioa83a268s8'
+        MAIN_API: 'https://sheetdb.io/api/v1/j29539mbwzs2c',
+        ADMIN_API: 'https://sheetdb.io/api/v1/j29539mbwzs2c'
     },
     
     // Storage keys
